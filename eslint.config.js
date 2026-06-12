c6bc7bbcb9743a4e3db4287ca9e210be0b5d4ca7
rajveer3x <panwarrajveer07@gmail.com>
2026-06-12 12:55:52 +0530

    first commit

---
+++ eslint.config.js
@@ -0,0 +1,45 @@
+import eslint from "@eslint/js";
+import prettierRecommended from "eslint-plugin-prettier/recommended";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  {
+    ignores: [
+      "**/coverage/**",
+      "**/dist/**",
+      "**/node_modules/**"
+    ]
+  },
+  eslint.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  ...tseslint.configs.stylisticTypeChecked,
+  {
+    files: [
+      "**/*.ts",
+      "**/*.tsx",
+      "**/*.mts",
+      "**/*.cts"
+    ],
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname
+      }
+    }
+  },
+  {
+    files: [
+      "**/*.js",
+      "**/*.mjs",
+      "**/*.cjs"
+    ],
+    extends: [
+      tseslint.configs.disableTypeChecked
+    ],
+    languageOptions: {
+      globals: globals.node
+    }
+  },
+  prettierRecommended
+);
